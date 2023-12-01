3b7a2f24b74c9082ea05224f9c92c36a1665d72e
Taylor Pool <taylorpool.27@gmail.com>
2023-11-30 23:46:46 -0500

    Added exploration rnd model

---
+++ gp4expl/exploration/rnd_model.py
@@ -0,0 +1,60 @@
+from cs16831.hw4_part2.infrastructure import pytorch_util as ptu
+from .base_exploration_model import BaseExplorationModel
+import torch.optim as optim
+from torch import nn
+import torch
+
+def init_method_1(model):
+    model.weight.data.uniform_()
+    model.bias.data.uniform_()
+
+def init_method_2(model):
+    model.weight.data.normal_()
+    model.bias.data.normal_()
+
+
+class RNDModel(nn.Module, BaseExplorationModel):
+    def __init__(self, hparams, optimizer_spec, **kwargs):
+        super().__init__(**kwargs)
+        self.ob_dim = hparams['ob_dim']
+        self.output_size = hparams['rnd_output_size']
+        self.n_layers = hparams['rnd_n_layers']
+        self.size = hparams['rnd_size']
+        self.optimizer_spec = optimizer_spec
+
+        # <TODO>: Create two neural networks:
+        # 1) f, the random function we are trying to learn
+        # 2) f_hat, the function we are using to learn f
+        self.f = ptu.build_mlp(self.ob_dim, self.output_size, self.n_layers, self.size, activation="relu")
+        init_method_1(self.f)
+        self.f.to(ptu.device)
+
+        self.f_hat = ptu.build_mlp(self.ob_dim, self.output_size, self.n_layers, self.size, activation="relu")
+        init_method_2(self.f_hat)
+        self.f_hat.to(ptu.device)
+
+        self.optimizer = optim.Adam(self.f_hat.parameters(), **self.optimizer_spec)
+        self.loss = nn.MSELoss()
+
+
+    def forward(self, ob_no):
+        # <TODO>: Get the prediction error for ob_no
+        # HINT: Remember to detach the output of self.f!
+        f_output = self.f(ob_no)
+        f_output.detach()
+        f_hat_output = self.f_hat(ob_no)
+        return f_hat_output - f_output
+
+    def forward_np(self, ob_no):
+        ob_no = ptu.from_numpy(ob_no)
+        error = self(ob_no)
+        return ptu.to_numpy(error)
+
+    def update(self, ob_no):
+        # <TODO>: Update f_hat using ob_no
+        # Hint: Take the mean prediction error across the batch
+        self.optimizer.zero_grad()
+        error = self.forward(ob_no)
+        loss = self.loss(error, torch.zeros_like(error))
+        loss.backward()
+        self.optimizer.step()
